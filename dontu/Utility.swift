27606c66e487a9e2428a938f6a16799d85539413
Maggie Marple <oldauntie@oldauntie.org>
2019-08-09 19:49:28 +0200

    Initial commit

---
+++ dontu/Utility.swift
@@ -0,0 +1,18 @@
+//
+//  Oldauntie.swift
+//  dontu
+//
+//  Created by nanna on 09/08/2019.
+//  Copyright © 2019 OldAuntie. All rights reserved.
+//
+
+import Foundation
+
+func d(what: Any, label: String = "DEBUG")
+{
+    print( "\(label)" + "\n" + "\(type(of: what)): " + "\(what)" )
+}
+
+
+
+
--- dontu/ViewController.swift
+++ dontu/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func presse(sender: UIButton)
+    {
+        d(what: "Camel are great!");
     }
 
 
