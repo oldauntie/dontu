0e27a86ea5f798b04017df3da0cf1930d4f7d978
Maggie Marple <oldauntie@oldauntie.org>
2019-08-10 15:55:11 +0200

    Button modified to hold a check/uncheck value. Added button for settigs

--- dontu/ViewController.swift
+++ dontu/ViewController.swift
@@ -18,22 +18,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    private var isArmed: Bool = false;
     
     @IBAction func SetChildAlarm(sender: UIButton)
     {
-        d(what: "Child")
+        ChangeState(sender: sender);
     }
     
     
     @IBAction func SetPetAlarm(sender: UIButton)
     {
-        d(what: "Pet")
+        ChangeState(sender: sender);
+        d(what: "Pet" + String(sender.isSelected));
     }
     
     
     @IBAction func SetOtherAlarm(sender: UIButton)
     {
-        d(what: "Other")
+        d(what: "Other");
+        ChangeState(sender: sender);
+    }
+    
+    
+    private func ChangeState(sender: UIButton)
+    {
+        sender.isSelected = !sender.isSelected;
+        
+        if(sender.isSelected == true)
+        {
+            // sender.setImage(UIImage(named:"pets"), for: UIControl.State.normal);
+            sender.backgroundColor = UIColor.orange;
+        }
+        else
+        {
+            // sender.setImage(UIImage(named:"child_friendly"), for: UIControl.State.normal);
+            sender.backgroundColor = UIColor.white;
+        }
     }
 
 
