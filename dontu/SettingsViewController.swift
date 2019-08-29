2842d0258e4082d83b6ba4b52d2b1c70712157da
Maggie Marple <oldauntie@oldauntie.org>
2019-08-29 18:29:02 +0200

    Aggiunto controllo su sorgente audio

--- dontu/SettingsViewController.swift
+++ dontu/SettingsViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SettingsViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var available_portname: UITextField!
+    @IBOutlet weak var available_porttype: UILabel!
+    @IBOutlet weak var available_uid: UILabel!
+    
+    @IBOutlet weak var selected_portname: UITextField!
+    @IBOutlet weak var selected_uid: UILabel!
+    
     @IBOutlet weak var distance: UISegmentedControl!
     @IBOutlet weak var other: UITextField!
+    
+    
+    @IBAction func useTouchUpInside(_ sender: Any) {
+        selected_portname.text = available_portname.text
+        selected_uid.text = available_uid.text
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,18 +42,60 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let userDefaults = UserDefaults.standard
         
+        
+        let avsession = AVAudioSession.sharedInstance()
+        
+        try! avsession.setCategory(AVAudioSession.Category.playAndRecord, options: .allowBluetooth)
+        try! avsession.setActive(false)
+        
+        let route  = avsession.currentRoute
+        
+        print("route:::::: \(route.outputs.debugDescription) \n")
+        let out = route.outputs
+        for element in out{
+            print("portname: \(element.portName)")
+            
+            if element.portType == AVAudioSession.Port.bluetoothLE{
+                print("out BLE")
+            }
+            
+            if element.portType == AVAudioSession.Port.bluetoothA2DP{
+                print("out A2DP")
+            }
+            
+            if element.portType == AVAudioSession.Port.bluetoothHFP{
+                print("out HFP")
+            }
+            
+            print("rawValue: \(element.portType.rawValue) UID: \(element.uid) ")
+            
+            
+            
+            available_portname.text = element.uid
+            available_porttype.text = "Type: \(element.portType.rawValue)"
+            available_uid.text = "UID: \(element.uid)"
+            
+        }
+                
+        // load user default in settings form
+        let userDefaults = UserDefaults.standard
         other.text = userDefaults.object(forKey: "Other") as? String
         distance.selectedSegmentIndex  = userDefaults.integer(forKey: "Distance")
+        
+        selected_portname.text = userDefaults.object(forKey: "PortName") as? String
+        selected_uid.text = userDefaults.object(forKey: "UID") as? String
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
+        // save user defaul from settings
         let userDefaults = UserDefaults.standard
         userDefaults.set(distance.selectedSegmentIndex, forKey: "Distance")
         userDefaults.set(other.text, forKey: "Other")
+        userDefaults.set(selected_portname.text, forKey: "PortName")
+        userDefaults.set(selected_uid.text, forKey: "UID")
     }
     
     
